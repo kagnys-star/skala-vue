a5a966d60e414040c294c2887249d9b563a5209d
KangYuseong <reky@g.skku.edu>
2026-07-31 16:35:22 +0900

    main

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SampleOne from './sampleOne.vue'
+import SampleTwo from './sampleTwo.vue'
+</script>
+
+<template>
+  <div class="app">
+    <SampleOne />
+    <SampleTwo />
+  </div>
+</template>
+
+<style scoped>
+.app {
+  display: flex;
+  gap: 2rem;
+  flex-wrap: wrap;
+}
+</style>
